662d60c255b98cb4900f13e20932776f36889563
tuandang <tuandangvan1699@gmail.com>
2022-07-21 16:28:18 +0700

    update Table

--- web_advanced/homework/hw_ad2_createTable/main.js
+++ web_advanced/homework/hw_ad2_createTable/main.js
@@ -1,94 +1,97 @@
-// function createTable() {
-//   const table = document.querySelector("table");
-//   const thead = document.querySelector("thead");
-
-//   for (let i = 0; i < 3; i++) {
-//     const tr = thead.insertRow();
-//     for (let j = 0; j < 2; j++) {}
-//   }
-// }
-
-// const data = [
-
-//   [1, "Giang Hồng Sơn", "1000000", "Sugar Daddy"],
-
-//   [2, "Nguyễn Thế Anh", "200000", "Sugar Brother"],
-
-//   [3, "Nguyễn Thế Anh", "200000", "Sugar Brother"],
-// ];
-
-// const table = document.querySelector("table");
-// const thead = document.querySelector("thead");
-// const tbody = document.querySelector("tbody");
-
-// function creatThead(data) {
-//   const th = thead.insertRow();
-
-//   const datathead = data[0];
-
-//   for (j = 0; j < datathead.length; j++) {
-//     const cell = th.insertCell(j);
-//     cell.appendChild(document.createTextNode(datathead[j]));
-//   }
-// }
-
-// creatThead(data);
-
-// function creatBody(data) {
-//   const result = data.forEach(function (paticipant) {
-//     const tr = tbody.insertRow();
-//     for (i = 0; i < paticipant.length; i++) {
-//       const cell = tr.insertCell(i);
-//       cell.appendChild(document.createTextNode(paticipant[i]));
-//     }
-//   });
-// }
-
-// creatBody(data);
-
-// const tr = thead.insertRow();
-// for (let i = 0; i < 3; i++) {
-//   const cell = tr.insertCell(i);
-//   cell.appendChild(document.createTextNode("test:" + i));
-// }
-
 const table = document.querySelector("table");
 const thead = document.querySelector("thead");
+const tbody = document.querySelector("tbody");
+const body = document.querySelector("body");
 
 const datathead = ["STT", "Tên", "Mức đóng góp", "Nhận xét"];
-
+const dataKey = ["stt", "name", "donate", "note"];
 const databody = [
-  [1, "Giang Hồng Sơn", "1000000", "Sugar Daddy"],
-
-  [2, "Nguyễn Thế Anh", "200000", "Sugar Brother"],
-
-  [3, "Nguyễn Thế Anh", "200000", "Sugar Brother"],
+  {
+    stt: 1,
+    name: "Giang Hồng Sơn",
+    donate: 1000000,
+    note: "Sugar Daddy",
+  },
+  {
+    stt: 2,
+    name: "Nguyễn Thế Anh",
+    donate: 200000,
+    note: "Sugar Brother",
+  },
+  {
+    stt: 3,
+    name: "Bùi Lan Nhi",
+    donate: 100000,
+    note: "Định mời Nhi ăn tối hóa ra lại thành bữa tối của Nhi",
+  },
+  {
+    stt: 4,
+    name: "Nguyễn Hữu Hà",
+    donate: 150000,
+    note: "Tuổi trẻ tài cao :D",
+  },
+  {
+    stt: 5,
+    name: "Phạm Hồng Anh",
+    donate: 100000,
+    note: "Lúc đầu gáy to, về sau hơi rén :))",
+  },
+  {
+    stt: 6,
+    name: "Nguyễn Văn Hùng",
+    donate: 200000,
+    note: "Kèo nào cũng cân",
+  },
+  {
+    stt: 7,
+    name: "Bùi Minh Đức",
+    donate: 100000,
+    note: "Hơi rén, lần sau cần mạnh dạn hơn",
+  },
+  {
+    stt: 8,
+    name: "Nguyễn Văn Tùng",
+    donate: 200000,
+    note: "Bị buồn =]]",
+  },
+  {
+    stt: 9,
+    name: "Dương Đức Thịnh",
+    donate: 100000,
+    note: "Mồm to húp hết nửa bill @@",
+  },
+  {
+    stt: 10,
+    name: "Thầy giá Barr",
+    donate: 100000,
+    note: "Cám ơn Nhi đã mời mình :)",
+  },
 ];
 
-function createthead(datathead) {
-  const thr = document.createElement("tr");
+function creatTable(datathead, databody, dataKey) {
+  let thr = document.createElement("tr");
   datathead.forEach(function (title) {
     const th = document.createElement("th");
     th.appendChild(document.createTextNode(title));
     thr.appendChild(th);
   });
-  return thr;
-}
-// console.log(createthead(datathead));
 
-function creatBody(databody) {
-  const tbody = document.querySelector("tbody");
-  const tbr = document.createElement("tr");
+  thead.appendChild(thr);
 
-  databody.forEach(function (dataTable) {
-    let td = document.createElement("td");
-    for (i = 0; i < dataTable.length; i++) {
+  databody.forEach(function (man) {
+    let tr = document.createElement("tr");
+    dataKey.forEach(function (key) {
       let td = document.createElement("td");
-      td.appendChild(document.createTextNode(dataTable[i]));
-      tbr.appendChild(td);
-    }
+      td.appendChild(document.createTextNode(man[key]));
+      tr.appendChild(td);
+    });
+    tbody.appendChild(tr);
   });
-
-  return tbr;
 }
-console.log(creatBody(databody));
+
+creatTable(datathead, databody, dataKey);
+
+const text = document.createElement("div");
+text.innerHTML =
+  "<p class='right'><i>Tổng bill ca 1: 2022000</i> </br> <i>Tổng bill ca 2</i>: 105000 </br> <i>Tổng thiệt hại: 2127000</i> </br> <i>Tổng đóng góp: <b>2150000</b> </i></p>";
+body.append(text);
